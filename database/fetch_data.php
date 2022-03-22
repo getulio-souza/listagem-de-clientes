f41222355619a2da3c5cad880d80b5f15fcbcdc0
Getúlio Souza <getuliosouza@live.com>
2022-03-22 19:22:41 -0300

    codigo php avançou mas está com erro 404 method not allowed

--- database/fetch_data.php
+++ database/fetch_data.php
@@ -22,9 +22,35 @@ else{
 }
 
 if($_POST['length'] != -1){
-  $start = $_POST['start']
+  $start = $_POST['start'];
+  $length = $_POST['length'];
+  $sql .= " LIMIT ".$start.", ".$length;
 }
 
+$data = array();
 
+$run_query = mysqli_query($con, $sql);
+$filtered_rows = mysqli_num_rows($run_query);
+
+while($row = mysqli_fetch_assoc($run_query)){
+    $subarray = array();
+    $subarray[] = $row['id'];
+    $subarray[] = $row['nome'];
+    $subarray[] = $row['email'];
+    $subarray[] = $row['telefone'];
+    $subarray[] = $row['cidade'];
+    $subarray[] = '<a href="javascript:void();" class= "btn btn-sm btn-info">Editar</a> <a href="javascript:void();" class= "btn btn-sm btn-danger">Deletar</a>';
+    $data[] = $subarray;
+}
+
+$output = array(
+    'data'=>$data,
+    'draw'=>intval($_POST['draw']),
+    'recordsTotal'=>$count_all_rows,
+    'recordsFiltered'=>$filtered_rows,
+);
+
+echo json_encode($output);
 
 ?>
+
